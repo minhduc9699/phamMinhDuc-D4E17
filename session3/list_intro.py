3b8eb1e6cd1d73bd89b0aef57d3657f3e30adcb8
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-03 20:21:46 +0700

    session3

--- session3/list_intro.py
+++ session3/list_intro.py
@@ -5,7 +5,21 @@ quan_ao2 = 'ao phong'
 quan_ao3 = 'quan bo'
 
 list_quan_ao = ['hoodie', 'ao phong', 'quan bo', 'quần què']
+
 list_quan_ao.append('áo ba lỗ')  # Create
+
+index = list_quan_ao.index('quần què')  # find index of item
+
+list_quan_ao[index] = 'áo mới' # update
+
+removed_item = list_quan_ao.pop(0) # remove item and save it 
+print(removed_item)
+
+list_quan_ao.remove('quan bo') # remove by item value
+
+if 'ao phong' in list_quan_ao: # check if item in list
+    print('yeayy')
+
 len_list_quan_ao = len(list_quan_ao)
 
 for i in range(len_list_quan_ao): # Read all
