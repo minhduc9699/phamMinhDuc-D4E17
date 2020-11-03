3e9869e0a7cf66d6a734d826e3366fd78c493719
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-03 20:00:06 +0700

    session3

---
+++ session3/list_intro.py
@@ -0,0 +1,23 @@
+# List
+
+quan_ao1 = 'hoodie'
+quan_ao2 = 'ao phong'
+quan_ao3 = 'quan bo'
+
+list_quan_ao = ['hoodie', 'ao phong', 'quan bo', 'quần què']
+list_quan_ao.append('áo ba lỗ')  # Create
+len_list_quan_ao = len(list_quan_ao)
+
+for i in range(len_list_quan_ao): # Read all
+    print('item', list_quan_ao[i]) # Read
+
+for item in list_quan_ao: # Read all
+    print(item)
+
+
+# Create Read Update Delete
+# print(list_quan_ao)
+
+# print(quan_ao1)
+# print(quan_ao2)
+# print(quan_ao3)
