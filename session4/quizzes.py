2a53641344875ce6015af00af6390473db95567d
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-10 22:01:16 +0700

    session4

--- session4/quizzes.py
+++ session4/quizzes.py
@@ -1,31 +1,22 @@
-quizzes = [
-    {
-        'question': 'Con chó có mấy chân?',
-        'awnser': 3,
-        'choices': [
-            '2 chân', 
-            '3 chân', 
-            '1 chân', 
-            '4 chân'
-        ]   
-    },
-    {
-        'question': 'Con vịt có mấy chân?',
-        'awnser': 0,
-        'choices': [
-            '1 chân',
-            '3 chân',
-            '2 chân',
-            '4 chân'
-        ]
-    }
-]
+import pyexcel
 
-print(quiz['question'])
-for i in range(len(quiz['choices'])):
-    print(f'{i+1}: {quiz["choices"][i]}')
-use_choice = int(input('Enter correct position')) - 1
-if use_choice == quiz['awnser']:
-    print('correct!!')
-else:
-    print('wrong!!')
+quizzes = pyexcel.get_records(file_name='quiz.xls')
+for quiz in quizzes:
+    quiz['choices'] = quiz['choices'].split(',')
+    quiz['choices'].pop(-1)
+print(quizzes[1]['choices'])
+
+count = 0 
+for quiz in quizzes:
+    print(quiz['question'])
+    for i in range(len(quiz['choices'])):
+        print(f'{i+1}: {quiz["choices"][i]}')
+    use_choice = int(input('Enter correct position')) - 1
+    if use_choice == quiz['awnser']:
+        print('correct!!')
+        count = count + 1
+    else:
+        print('wrong!!')
+
+score = (count * 100) / len(quizzes)
+print(f'your score is {score}%')
