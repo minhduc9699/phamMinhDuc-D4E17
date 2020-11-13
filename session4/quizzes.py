e0970e96d62bb016453d59df29f8f64fcf7016f8
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-13 21:04:55 +0700

    session 5

--- session4/quizzes.py
+++ session4/quizzes.py
@@ -1,10 +1,18 @@
-import pyexcel
+from pymongo import MongoClient
 
-quizzes = pyexcel.get_records(file_name='quiz.xls')
-for quiz in quizzes:
-    quiz['choices'] = quiz['choices'].split(',')
-    quiz['choices'].pop(-1)
-print(quizzes[1]['choices'])
+client = MongoClient('localhost', 27017)
+
+quiz_database = client.get_database('quiz')
+
+quizzes_collection = quiz_database.get_collection('quizzes')
+score_collection = quiz_database.get_collection('scores')
+# quizzes = pyexcel.get_records(file_name='quiz.xls')
+quizzes = quizzes_collection.find()
+total_quizzes = quizzes_collection.count_documents({})
+# for quiz in quizzes:
+#     quiz['choices'] = quiz['choices'].split(',')
+#     quiz['choices'].pop(-1)
+# print(quizzes[1]['choices'])
 
 count = 0 
 for quiz in quizzes:
@@ -18,5 +26,8 @@ for quiz in quizzes:
     else:
         print('wrong!!')
 
-score = (count * 100) / len(quizzes)
-print(f'your score is {score}%')
+score = (count * 100) / total_quizzes
+score_collection.insert_one({
+    "name": "Duc",
+    "score": score
+})
