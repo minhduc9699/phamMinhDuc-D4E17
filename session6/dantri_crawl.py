f083bd4a79887cb6848ff3e040d06eae88713d08
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-24 20:46:46 +0700

    session 6

--- session6/dantri_crawl.py
+++ session6/dantri_crawl.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+dantri_database = client.get_database('dantri')
+highlight_collection = dantri_database.get_collection('highlights')
 
 web = requests.get('https://dantri.com.vn/')
 
@@ -10,5 +15,9 @@ title_list = highlight_area.find_all('li')
 
 for title in title_list:
     title_a_tag = title.find('a')
-    print(title_a_tag.text.strip())
-    print(title_a_tag['href'])
+    data = {
+        'title': title_a_tag.text.strip(),
+        'link': title_a_tag['href']
+    }
+    highlight_collection.insert_one(data)
+    
