ae8a7d3af1e79b39a523364b676fd223a045f049
Phạm Đức <minhduc.096.99@gmail.com>
2020-11-24 20:28:12 +0700

    session 6

--- session6/dantri_crawl.py
+++ session6/dantri_crawl.py
@@ -6,6 +6,9 @@ web = requests.get('https://dantri.com.vn/')
 content = BeautifulSoup(web.text)
 
 highlight_area = content.find('div', {'class': 'highlight-event'})
-
 title_list = highlight_area.find_all('li')
-print(title_list)
+
+for title in title_list:
+    title_a_tag = title.find('a')
+    print(title_a_tag.text.strip())
+    print(title_a_tag['href'])
